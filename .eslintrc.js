5a945b6335f0f3601ea62b77f21c8f07abcfc000
MynameisIM <dev@idem.agency>
2023-07-03 18:01:06 +0400

    sync

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,41 +4,31 @@ module.exports = {
     es2021: true,
     jest: true
   },
-  extends: [
-    'standard-with-typescript',
-    'plugin:react/recommended',
-    'plugin:i18next/recommended'
-  ],
-  overrides: [
-    {
-      env: {
-        node: true
-      },
-      files: [
-        '.eslintrc.{js,cjs}'
-      ],
-      parserOptions: {
-        sourceType: 'script'
-      }
+  extends: ['standard-with-typescript', 'plugin:react/recommended', 'plugin:i18next/recommended', 'plugin:storybook/recommended'],
+  overrides: [{
+    env: {
+      node: true
     },
-    {
-      files: ['**/src/**/*.test.{ts, tsx}'],
-      rules: {
-        'i18next/no-literal-string': 'off',
-      }
+    files: ['.eslintrc.{js,cjs}'],
+    parserOptions: {
+      sourceType: 'script'
     }
-  ],
+  }, {
+    files: ['**/src/**/*.test.{ts, tsx}'],
+    rules: {
+      'i18next/no-literal-string': 'off'
+    }
+  }],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
-  plugins: [
-    'react',
-    'i18next',
-  ],
+  plugins: ['react', 'i18next'],
   rules: {
     'react/jsx-indent': ["error", 2],
-    'react/jsx-filename-extension': [2, { "extensions": [".js", ".jsx", ".ts", ".tsx"] }],
+    'react/jsx-filename-extension': [2, {
+      "extensions": [".js", ".jsx", ".ts", ".tsx"]
+    }],
     'import/no-unresolved': 'off',
     'import/prefer-default-export': 'off',
     'no-unused-vars': 'warn',
@@ -50,10 +40,15 @@ module.exports = {
     'import/extensions': 'off',
     'import/no-extraneous-dependencies': 'off',
     'no-underscore-dangle': 'off',
-    'i18next/no-literal-string': ['error', { markupOnly: true, ignoreAttribute: ['data-testid', 'to'] }],
-    'max-len': ['error', { "ignoreComments": true }]
+    'i18next/no-literal-string': ['error', {
+      markupOnly: true,
+      ignoreAttribute: ['data-testid', 'to']
+    }],
+    'max-len': ['error', {
+      "ignoreComments": true
+    }]
   },
   globals: {
-    '__ID_DEV__': true,
+    '__ID_DEV__': true
   }
-}
+};
---
+++ config/build/loaders/buildCssLoader.ts
@@ -0,0 +1,20 @@
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+
+export function buildCssLoader(options) {
+  return {
+    test: /\.s[ac]ss$/i,
+    use: [
+      options.isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+      {
+        loader: "css-loader",
+        options: {
+          modules: {
+            auto: (resPath: string) => resPath.includes('.module.'),
+            localIdentName: options.isDev ? '[path][name]__[local]--[hash:base64:5]' : '[hash:base64:8]'
+          },
+        }
+      },
+      "sass-loader"
+    ]
+  }
+}
