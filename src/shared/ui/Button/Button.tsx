f9eab4fe840439ad99abcfbefa40b682fc705768
MynameisIM <dev@idem.agency>
2023-07-05 10:10:14 +0400

    lint fix

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -5,6 +5,8 @@ import cls from './Button.module.scss';
 export enum ThemeButton {
   CLEAR = 'clear',
   OUTLINE = 'outline',
+  BACKGROUND = 'background',
+  BACKGROUND_INVERTED = 'backgroundInverted'
 }
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
--- src/widgets/Sidebar/ui/SideBar/SideBar.tsx
+++ src/widgets/Sidebar/ui/SideBar/SideBar.tsx
@@ -3,6 +3,7 @@ import {classNames} from "shared/lib/classNames/classNames";
 import cls from './SideBar.module.scss';
 import {ThemeSwitcher} from "widgets/ThemeSwitcher/ui/ThemeSwitcher";
 import {LangSwitcher} from "widgets/LangSwitcher/LangSwitcher";
+import {Button} from "shared/ui/Button/Button";
 
 interface SideBarProps {
   className?: string,
@@ -16,7 +17,7 @@ export const SideBar = ({ className }: SideBarProps) => {
 
   return (
     <div data-testid='sidebar' className={classNames(cls.SideBar, {[cls.collapsed]: collapsed}, [className])}>
-      <button data-testid='sidebar-toggle' onClick={onToggle}>toggle</button>
+      <Button data-testid='sidebar-toggle' onClick={onToggle} className={cls.collapseBtn}>{collapsed ? '>' : '<'}</Button>
       <div className={cls.switchers}>
         <ThemeSwitcher/>
         <LangSwitcher className={cls.lang}/>
