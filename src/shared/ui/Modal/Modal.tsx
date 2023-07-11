d026fb4c669b729b9fa4f465a15b272cae7d1c56
MynameisIM <dev@idem.agency>
2023-07-11 12:11:27 +0400

    sync

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -1,7 +1,8 @@
-import React, {ReactNode, useCallback, useEffect, useRef, useState} from 'react';
+import React, {ReactNode, useCallback, useContext, useEffect, useRef, useState} from 'react';
 import {classNames} from "shared/lib/classNames/classNames";
 import cls from './Modal.module.scss';
 import {Portal} from "shared/ui/Portal/Portal";
+import {ThemeContext} from "app/providers/ThemeProvider/lib/ThemeContext";
 
 interface ModalProps {
   className?: string,
@@ -15,6 +16,7 @@ const ANIMATION_DELAY = 300;
 export const Modal = ({ className, children, isOpen, onClose }: ModalProps) => {
   const [isClosing, setIsClosing] = useState(false);
   const timerRef = useRef<ReturnType<typeof setTimeout>>();
+  const { theme } = useContext(ThemeContext);
 
   const mods: Record<string, boolean> = {
     [cls.opened]: isOpen,
@@ -53,7 +55,7 @@ export const Modal = ({ className, children, isOpen, onClose }: ModalProps) => {
 
   return (
     <Portal>
-      <div className={classNames(cls.Modal, mods, [className])}>
+      <div className={classNames(cls.Modal, mods, [className, theme])}>
         <div className={cls.overlay} onClick={closeHandler}>
           <div className={cls.content} onClick={onContentClick}>
             { children }
