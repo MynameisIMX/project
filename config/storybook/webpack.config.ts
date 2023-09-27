e68262f9666a6ce22735edab5b9de7b30d160796
MynameisIM <dev@idem.agency>
2023-09-27 18:02:52 +0400

    sync

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -10,21 +10,21 @@ export default ({config} : {config: webpack.Configuration}) => {
     entry: '',
     src: path.resolve(__dirname, '..', '..', 'src'),
   }
-  config.resolve.modules.push(paths.src);
-  config.resolve.extensions.push('.ts', '.tsx');
-  config.module.rules = config.module.rules.map((rule: RuleSetRule) => {
+  config!.resolve!.modules.push(paths.src);
+  config!.resolve!.extensions.push('.ts', '.tsx');
+  config!.module!.rules = config!.module!.rules.map((rule: RuleSetRule) => {
     if (/svg/.test(rule.test)) {
       return { ...rule, exclude: /\.svg/i };
     }
     return rule;
   });
-  config.module.rules.push({
+  config!.module!.rules.push({
     test: /\.svg$/,
     use: ['@svgr/webpack']
   });
-  config.module.rules.push(buildCssLoader({isDev: true}));
+  config!.module!.rules.push(buildCssLoader({isDev: true}));
 
-  config.plugins.push(new webpack.DefinePlugin({
+  config!.plugins!.push(new webpack.DefinePlugin({
     __IS_DEV__: true,
     __API__: JSON.stringify(''),
   }))
