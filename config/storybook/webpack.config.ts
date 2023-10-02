eaa124d73b6c86d797faa3b9205007a85d6d7bf7
MynameisIM <dev@idem.agency>
2023-10-02 10:44:45 +0400

    sync\

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -10,8 +10,9 @@ export default ({config} : {config: webpack.Configuration}) => {
     entry: '',
     src: path.resolve(__dirname, '..', '..', 'src'),
   }
-  config!.resolve!.modules.push(paths.src);
-  config!.resolve!.extensions.push('.ts', '.tsx');
+  config!.resolve!.modules!.push(paths.src);
+  config!.resolve!.extensions!.push('.ts', '.tsx');
+  // @ts-ignore
   config!.module!.rules = config!.module!.rules.map((rule: RuleSetRule) => {
     if (/svg/.test(rule.test)) {
       return { ...rule, exclude: /\.svg/i };
