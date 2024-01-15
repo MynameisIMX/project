ad07308ff7e26553418a4846f3f81dc9b65fb1cd
MynameisIM <dev@idem.agency>
2024-01-15 15:39:29 +0400

    sync

--- .eslintrc.js
+++ .eslintrc.js
@@ -51,7 +51,8 @@ module.exports = {
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'error',
     'no-param-reassign': 'off',
-    'no-undef': 'off'
+    'no-undef': 'off',
+    'react/no-array-index-key': 'off',
   },
   globals: {
     '__ID_DEV__': true,
---
+++ src/pages/AboutPage/ui/ArticlesPage.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import { memo } from 'react';
+import { ArticleList, ArticleView } from 'entities/Article';
+import cls from './ArticlesPage.module.scss';
+
+interface ArticlesPageProps {
+    className?: string;
+}
+
+const ArticlesPage = (props: ArticlesPageProps) => {
+    const { className } = props;
+    const { t } = useTranslation();
+
+    return (
+        <div className={classNames(cls.ArticlesPage, {}, [className])}>
+            <ArticleList
+                isLoading
+                view={ArticleView.BIG}
+                articles={[]}
+            />
+        </div>
+    );
+};
+
+export default memo(ArticlesPage);
