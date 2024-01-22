8f73e94959f6ccdb8fc708b0dc8d6d2e62eec09d
MynameisIM <dev@idem.agency>
2024-01-22 17:26:00 +0400

    sync

--- src/app/providers/StoreProvider/config/StateSchema.ts
+++ src/app/providers/StoreProvider/config/StateSchema.ts
@@ -1,16 +1,13 @@
-import { CounterSchema } from 'entities/Counter';
-import { UserSchema } from 'entities/User';
-import { LoginSchema } from 'features/AuthByUsername';
-import {
-  AnyAction, EnhancedStore, Reducer, ReducersMapObject,
-} from '@reduxjs/toolkit';
-import { CombinedState, Dispatch } from 'redux';
-import { ProfileSchema } from 'entities/Profile';
-import { AxiosInstance } from 'axios';
-import { To, NavigateOptions } from 'react-router';
-import { AppDispatch } from 'app/providers/StoreProvider';
-import { ArticleDetailsSchema } from "entities/Article";
-import { ArticleDetailsCommentsSchema } from 'pages/ArticleDetailsPage';
+import {CounterSchema} from 'entities/Counter';
+import {UserSchema} from 'entities/User';
+import {LoginSchema} from 'features/AuthByUsername';
+import {AnyAction, EnhancedStore, Reducer, ReducersMapObject,} from '@reduxjs/toolkit';
+import {CombinedState} from 'redux';
+import {ProfileSchema} from 'entities/Profile';
+import {AxiosInstance} from 'axios';
+import {NavigateOptions, To} from 'react-router';
+import {ArticleDetailsSchema} from "entities/Article";
+import {ArticleDetailsCommentsSchema} from 'pages/ArticleDetailsPage';
 import {AddCommentFormSchema} from "features/addCommentForm";
 import {ArticlesPageSchema} from "pages/ArticlesPage";
 
@@ -28,12 +25,14 @@ export interface StateSchema {
 }
 
 export type StateSchemaKey = keyof StateSchema;
+export type MountedReducers = OptionalRecord<StateSchemaKey, boolean>;
 
 export interface ReducerManager {
   getReducerMap: () => ReducersMapObject<StateSchema>;
   reduce: (state: StateSchema, action: AnyAction) => CombinedState<StateSchema>;
   add: (key: StateSchemaKey, reducer: Reducer) => void;
   remove: (key: StateSchemaKey) => void;
+  getMountedReducers: () => MountedReducers;
 }
 
 export interface ReduxStoreWithManager extends EnhancedStore<StateSchema> {
--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
@@ -1,21 +1,20 @@
-import React from "react";
-import { classNames } from 'shared/lib/classNames/classNames';
-import { useTranslation } from 'react-i18next';
-import { memo, useCallback } from 'react';
-import { ArticleList, ArticleView, ArticleViewSelector } from 'entities/Article';
-import { DynamicModuleLoader, ReducersList } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
-import { useInitialEffect } from 'shared/lib/hooks/useInitialEffect/useInitialEffect';
-import { useDispatch, useSelector } from 'react-redux';
-import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
-import { Page } from 'shared/ui/Page/Page';
-import { fetchNextArticlesPage } from 'pages/ArticlesPage/model/services/fetchNextArticlesPage/fetchNextArticlesPage';
-import { fetchArticlesList } from '../../model/services/fetchArticlesList/fetchArticlesList';
-import { articlesPageActions, articlesPageReducer, getArticles } from '../../model/slices/articlesPageSlice';
+import React, {memo, useCallback} from "react";
+import {classNames} from 'shared/lib/classNames/classNames';
+import {useTranslation} from 'react-i18next';
+import {ArticleList, ArticleView, ArticleViewSelector} from 'entities/Article';
+import {DynamicModuleLoader, ReducersList} from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
+import {useInitialEffect} from 'shared/lib/hooks/useInitialEffect/useInitialEffect';
+import {useSelector} from 'react-redux';
+import {useAppDispatch} from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
+import {Page} from 'shared/ui/Page/Page';
+import {fetchNextArticlesPage} from 'pages/ArticlesPage/model/services/fetchNextArticlesPage/fetchNextArticlesPage';
+import {initArticlesPage} from '../../model/services/initArticlesPage/initArticlesPage';
+import {articlesPageActions, articlesPageReducer, getArticles} from '../../model/slices/articlesPageSlice';
 import cls from './ArticlesPage.module.scss';
 import {
-    getArticlesPageError, getArticlesPageHasMore,
-    getArticlesPageIsLoading, getArticlesPageNum,
-    getArticlesPageView,
+  getArticlesPageError,
+  getArticlesPageIsLoading,
+  getArticlesPageView,
 } from '../../model/selectors/articlesPageSelectors';
 
 interface ArticlesPageProps {
@@ -34,7 +33,6 @@ const ArticlesPage = (props: ArticlesPageProps) => {
     const isLoading = useSelector(getArticlesPageIsLoading);
     const view = useSelector(getArticlesPageView);
     const error = useSelector(getArticlesPageError);
-
     const onChangeView = useCallback((view: ArticleView) => {
         dispatch(articlesPageActions.setView(view));
     }, [dispatch]);
@@ -44,10 +42,7 @@ const ArticlesPage = (props: ArticlesPageProps) => {
     }, [dispatch]);
 
     useInitialEffect(() => {
-        dispatch(articlesPageActions.initState());
-        dispatch(fetchArticlesList({
-            page: 1,
-        }));
+      dispatch(initArticlesPage());
     });
 
     return (
